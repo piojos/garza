86a6685a6efa2ee4b2a35823362d7e1b5e3ec795
raidho-mx <miranda@raidho.mx>
2016-11-18 13:07:41 -0600

    Up to date with Linda & register menus

--- functions.php
+++ functions.php
@@ -8,12 +8,12 @@
 /* Proper way to enqueue scripts and styles */
 
 	function wpdocs_theme_name_scripts() {
+		wp_enqueue_style( 'Normalizer', get_template_directory_uri() . '/css/normalize.min.css');
 		wp_enqueue_style( 'Style', get_stylesheet_uri() );
-		wp_enqueue_style( 'Hint', get_template_directory_uri() . '/css/hint.min.css');
-		wp_enqueue_style( 'AOScss', '//cdn.rawgit.com/michalsnik/aos/2.0.4/dist/aos.css');
-		wp_enqueue_script( 'Slick', get_template_directory_uri() . '/js/slick.min.js', array('jquery') );
-		wp_enqueue_script( 'AOS', '//cdn.rawgit.com/michalsnik/aos/2.0.4/dist/aos.js');
-		wp_enqueue_script( 'Magic', get_template_directory_uri() . '/js/scripts.js');
+		wp_enqueue_style( 'Slick', get_template_directory_uri() . '/css/slick.css');
+		wp_enqueue_style( 'Shame', get_template_directory_uri() . '/css/shame.css');
+		wp_enqueue_script( 'Slickjs', '//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js', array('jquery') );
+		wp_enqueue_script( 'Magic', get_template_directory_uri() . '/js/scripts.js' );
 	}
 	add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
 
@@ -32,7 +32,6 @@ if (function_exists('add_theme_support'))
 
 	// Add Thumbnail Theme Support
 	add_theme_support('post-thumbnails');
-	add_image_size('food', 570, 450, true); // FOOD Thumbnail
 	// add_image_size('medium', 250, '', true); // Medium Thumbnail
 	// add_image_size('small', 120, '', true); // Small Thumbnail
 	// add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
@@ -48,38 +47,25 @@ if (function_exists('add_theme_support'))
 
 
 
-/*------------------------------------*\
-	Functions
-\*------------------------------------*/
+// Register Menus
 
-
-// HTML5 Blank navigation
-
-	function html5blank_nav()
-	{
-		wp_nav_menu(
-		array(
-			'theme_location'  => 'header-menu',
-			'menu'            => '',
-			'container'       => 'div',
-			'container_class' => 'menu-{menu slug}-container',
-			'container_id'    => '',
-			'menu_class'      => 'menu',
-			'menu_id'         => '',
-			'echo'            => true,
-			'fallback_cb'     => 'wp_page_menu',
-			'before'          => '',
-			'after'           => '',
-			'link_before'     => '',
-			'link_after'      => '',
-			'items_wrap'      => '<ul>%3$s</ul>',
-			'depth'           => 0,
-			'walker'          => ''
+	function register_my_menus() {
+		register_nav_menus(
+			array(
+				'header-menu' => __( 'Menu principal' ),
+				'extra-menu' => __( 'Menu secundario' ),
+				'footer-menu' => __( 'Menu inferior' )
 			)
 		);
 	}
+	add_action( 'init', 'register_my_menus' );
 
 
+
+
+/*------------------------------------*\
+	Functions
+\*------------------------------------*/
 
 
 // Add page slug to body class, love this - Credit: Starkers Wordpress Theme
@@ -105,7 +91,7 @@ if (function_exists('add_theme_support'))
 
 // Pagination for paged posts, Page 1, Page 2, Page 3, with Next and Previous Links, No plugin
 
-	function html5wp_pagination() {
+	function pagination() {
 		global $wp_query;
 		$big = 999999999;
 		echo paginate_links(array(
@@ -184,6 +170,18 @@ if (function_exists('add_theme_support'))
 
 
 
+// Cambiar Entradas a Descubre
+
+	function change_post_label() {
+		global $menu;
+		global $submenu;
+		$menu[5][0] = 'Descubre';
+	}
+	add_action( 'admin_menu', 'change_post_label' );
+
+
+
+
 /*------------------------------------*\
 	Theme specific Functions
 \*------------------------------------*/
@@ -192,9 +190,6 @@ if (function_exists('add_theme_support'))
 // Classes for blocks
 
 	function blockClass($op) {
-		if( get_row_layout() == 'promo' ) {
-			if(in_array('content', $op)) { $classes .= ' complex'; } else {$classes .= ' simple no_text'; }
-		}
 		if(in_array('push_top', $op)) $classes .= ' push_top';
 		if(in_array('push_bot', $op)) $classes .= ' push_bot';
 		if(in_array('pad_top', $op)) $classes .= ' pad_top';
