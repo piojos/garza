6ddf418fe795a9600040c6a775a782f0787ad759
raidho-mx <miranda@raidho.mx>
2016-12-02 12:48:19 -0600

    List Categories properly

--- loop.php
+++ loop.php
@@ -3,22 +3,26 @@
 	while (have_posts()) :
 		the_post();
 
+		$thisType = wp_get_post_terms(get_the_id(), 'tipos_de_proyectos');
 
 		?>
 
 	<div class="one-fourth columns">
 
 		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php
-		$thisType = wp_get_post_terms(get_the_id(), 'tipos_de_proyectos');
+
+
 		if(has_post_thumbnail()) { ?>
 			<div class="img" style="background-image: url('<?php the_post_thumbnail_url( 'medium' ); ?>');"></div><?php
 		} ?>
 			<div class="txt">
 				<h3><?php the_title(); ?></h3><?php
-		foreach( $thisType as $pType ) { ?>
-				<p class="small-txt c-blue mb20"><b><?php echo $pType->name; ?></b></p><?php
+
+
+		if($thisType) { ?>
+			<p class="small-txt c-blue mb20"><b><?php echo listCategories($thisType); ?></p><?php
 		} ?>
-				<div class="marquee"><p class="small-txt"><b><?php echo listColonias(get_field('zone')); ?></b></p></div>
+				<div class="marquee"><p class="small-txt"><b><?php echo listTitles(get_field('zone')); ?></b></p></div>
 			</div><?php
 
 
