9a1f5ab40f35d7c8a307b0221dede311c3595809
raidho-mx <miranda@raidho.mx>
2016-12-02 12:45:59 -0600

    Added Post type situation

--- inc/breadcrumbs.php
+++ inc/breadcrumbs.php
@@ -4,23 +4,26 @@
 	<ul>
 		<li><a href="#">Inicio</a></li><?php
 
-
-	if(is_singular('proyectos')) { ?>
-		<li><a href="#">Proyectos</a></li><?php
-		$pTypes = wp_get_post_terms(get_the_id(), 'tipos_de_proyectos');
-		foreach( $pTypes as $pType ) { ?>
-		<li><a href="<?php echo get_term_link($pType->term_id) ?>" title="<?php echo $pType->name ?>"><?php echo $pType->name; ?></a></li><?php
-		}
-
-
-	} elseif(is_single()) { ?>
-		<li><?php the_category(' '); ?></li>
-		<?php
-
-
+	if(is_archive()) { ?>
+		<li><a href="#" class="active"><?php post_type_archive_title(); ?></a></li><?php
 	} else {
-		echo 'no info';
+		if(is_singular('proyectos')) { ?>
+			<li><a href="#">Proyectos</a></li><?php
+			$pTypes = wp_get_post_terms(get_the_id(), 'tipos_de_proyectos');
+			foreach( $pTypes as $pType ) { ?>
+			<li><a href="<?php echo get_term_link($pType->term_id) ?>" title="<?php echo $pType->name ?>"><?php echo $pType->name; ?></a></li><?php
+			}
+
+
+		} elseif(is_single()) { ?>
+			<li><?php the_category(' '); ?></li>
+			<?php
+
+
+		} else {
+			echo 'no info';
+		} ?>
+			<li><a href="<?php the_permalink(); ?>" class="active"><?php the_title(); ?></a></li><?php
 	} ?>
-		<li><a href="<?php the_permalink(); ?>" class="active"><?php the_title(); ?></a></li>
 	</ul>
 </div>
