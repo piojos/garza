e25e4dbd252c13963b8f4d669dfcaaf90873bf0f
raidho-mx <miranda@raidho.mx>
2016-12-01 20:52:42 -0600

    updated markup, listed colonias, state

--- loop.php
+++ loop.php
@@ -1,5 +1,10 @@
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php
 
+	while (have_posts()) :
+		the_post();
+
+
+		?>
 
 	<div class="one-fourth columns">
 
@@ -8,23 +13,44 @@
 		if(has_post_thumbnail()) { ?>
 			<div class="img" style="background-image: url('<?php the_post_thumbnail_url( 'medium' ); ?>');"></div><?php
 		} ?>
-			<h3><?php the_title(); ?></h3><?php
+			<div class="txt">
+				<h3><?php the_title(); ?></h3><?php
 		foreach( $thisType as $pType ) { ?>
-			<p class="small-txt"><?php echo $pType->name; ?></p><?php
+				<p class="small-txt c-blue mb20"><b><?php echo $pType->name; ?></b></p><?php
 		} ?>
+				<div class="marquee"><p class="small-txt"><b><?php echo listColonias(get_field('zone')); ?></b></p></div>
+			</div><?php
+
+
+
+			while (have_rows('status')) {
+				the_row();
+				$opts = get_sub_field('options');
+				$prog = get_sub_field('percent');
+			}
+			if($prog) { ?>
+			<div class="thumb-progress-bar"><?php
+
+				if($opts['value'] == 'percent') {
+					$sLabel = '<b>'. $prog.'%</b> Completado';
+					$sPercent = $prog;
+				} elseif($opts['value'] == 'prox') {
+					$sLabel = '<b>'. $opts['label'] .'</b>';
+					$sPercent = 0;
+				} else {
+					$sLabel = '<b>'. $opts['label'] .'</b>';
+					$sPercent = 100;
+				} ?>
+
+				<p><?php echo $sLabel; ?></p>
+				<div style="width:<?php echo $sPercent; ?>%;" class="bg-blue"></div>
+			</div><?php
+			} ?>
 		</a>
 
 		<?php // edit_post_link(); ?>
 
-	</div>
-	<!-- /article -->
+	</div><?php
 
-<?php endwhile; ?>
 
-<?php else: /* ?>
-
-	<article>
-		<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-	</article><?php
-*/
-endif; ?>
+	endwhile; ?>
