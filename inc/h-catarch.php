55d31c79403418b7d1863f1a8e8970be464ef704
raidho-mx <miranda@raidho.mx>
2016-12-12 13:18:18 -0600

    Filter project categories

--- inc/h-catarch.php
+++ inc/h-catarch.php
@@ -1,15 +1,13 @@
-<?php
- ?>
 
-<section class="bg-blue">
+<section class="<?php echo bg_color(); ?>">
 	<div class="wrap">
 		<div class="two-col columns"><?php
+
 
 	if(is_post_type_archive()) {
 		$title = 'Proyectos en <br>Distritotec';
 		$legend = 'Tipos: ';
 		$description = 'Documentamos la mejora del entorno social en Distrito Tec con el campus y Cluster tecnológico.';
-		$thisType = get_terms('tipos_de_proyectos');
 	} elseif(is_archive()) {
 		$catID = get_queried_object_id();
 		$title = get_cat_name($catID);
@@ -23,21 +21,17 @@
 			<h1><b><?php echo $title; ?></b></h1>
 			<p class="mt20"><b><?php echo $legend; ?></b></p><?php
 
-	if($thisType) {
-		// echo '<pre>';
-		// print_r($thisType);
-		// echo '</pre>';
-		// $slug = get_terms('tipos_de_proyectos');
-		// $all = get_term_link($thisType, 'cluster');
-		// echo $all; ?>
-			<div class="filter-menu">
-				<select name="filter" onchange="location = this.value;"><?php
-		foreach( $thisType as $pType ) { ?>
-					<option value="<?php // echo get_term_link('tipos_de_proyectos', $pType->slug); ?>"><?php echo $pType->name; ?> (<?php echo $pType->count; ?>) </option><?php
-		} ?>
-				</select>
-			</div><?php
-	} ?>
+			$thisType = get_terms('tipos_de_proyectos');
+			if($thisType) { ?>
+					<div class="filter-menu">
+						<select name="filter" onchange="location = this.value;"><?php
+				foreach( $thisType as $pType ) {
+					print_r($pType); ?>
+							<option value="<?php echo get_term_link($pType); ?>"><?php echo $pType->name; ?> (<?php echo $pType->count; ?>) </option><?php
+				} ?>
+						</select>
+					</div><?php
+			} ?>
 		</div>
 		<div class="two-col columns">
 			<h1><?php echo $description; ?></h1>
