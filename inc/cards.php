089622bb1f263d87bce9cc311778629d9b362823
raidho-mx <miranda@raidho.mx>
2016-12-14 00:22:10 -0600

    Death to parent theme

---
+++ inc/cards.php
@@ -0,0 +1,52 @@
+<?php
+
+	$thisType = wp_get_post_terms(get_the_id(), 'tipos_de_proyectos');
+
+	?>
+
+	<div class="one-fourth columns">
+
+		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php
+
+
+		if(has_post_thumbnail()) { ?>
+			<div class="img" style="background-image: url('<?php the_post_thumbnail_url( 'medium' ); ?>');"></div><?php
+		} ?>
+			<div class="txt">
+				<h3><?php the_title(); ?></h3><?php
+		if($thisType) { ?>
+				<p class="small-txt c-blue mb20"><b><?php echo listCategories($thisType); ?></b></p><?php
+		} ?>
+				<div class="marquee"><p class="small-txt"><b><?php echo listTitles(get_field('zone')); ?></b></p></div>
+			</div><?php
+
+
+		while (have_rows('status')) {
+			the_row();
+			$opts = get_sub_field('options');
+			$prog = get_sub_field('percent');
+		}
+
+		if($opts['value'] == 'percent') {
+			$sLabel = '<b>'. $prog.'%</b> Completado';
+			$sPercent = $prog;
+		} elseif($opts['value'] == 'prox') {
+			$sLabel = '<b>'. $opts['label'] .'</b>';
+			$sPercent = 0;
+		} else {
+			$sLabel = '<b>'. $opts['label'] .'</b>';
+			$sPercent = 100;
+		}
+
+		if('proyectos' == get_post_type()) { ?>
+			<div class="thumb-progress-bar">
+				<p><?php echo $sLabel; ?></p>
+				<div style="width:<?php echo $sPercent; ?>%;" class="bg-blue"></div>
+			</div><?php
+		} ?>
+
+		</a>
+
+		<?php // edit_post_link(); ?>
+
+	</div>
