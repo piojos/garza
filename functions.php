f3dc9b15e189d54179bfff838b38bda7ec2598f5
raidho-mx <miranda@raidho.mx>
2017-01-04 17:23:58 -0600

    Added max img size, And delete images bigger than 2000px wide

--- functions.php
+++ functions.php
@@ -25,17 +25,16 @@
 \*------------------------------------*/
 
 
-if (function_exists('add_theme_support'))
-{
+if (function_exists('add_theme_support')) {
 	// Add Menu Support
 	add_theme_support('menus');
 
 	// Add Thumbnail Theme Support
 	add_theme_support('post-thumbnails');
 	add_image_size('medium', 450, '', true); // Medium Thumbnail
-	add_image_size('large', 1200, '', true); // Medium Thumbnail
+	add_image_size('large', 1200, '', true); // Large Thumbnail
+	add_image_size('huge', 2000, '', true); // Huge Thumbnail
 	// add_image_size('small', 120, '', true); // Small Thumbnail
-	// add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
 
 	// Enables post and comment RSS feed links to head
@@ -46,6 +45,41 @@ if (function_exists('add_theme_support'))
 }
 
 
+//    2 Borrar tamaño original de disco y opción
+
+	function replace_uploaded_image($image_data) {
+		// if there is no large image : return
+		if (!isset($image_data['sizes']['huge'])) return $image_data;
+
+		// paths to the uploaded image and the large image
+		$upload_dir = wp_upload_dir();
+		$uploaded_image_location = $upload_dir['basedir'] . '/' .$image_data['file'];
+		$large_image_filename = $image_data['sizes']['huge']['file'];
+
+		// Do what wordpress does in image_downsize() ... just replace the filenames ;)
+		$image_basename = wp_basename($uploaded_image_location);
+		$large_image_location = str_replace($image_basename, $large_image_filename, $uploaded_image_location);
+
+		// delete the uploaded image
+		unlink($uploaded_image_location);
+
+		// rename the large image
+		rename($large_image_location, $uploaded_image_location);
+
+		// update image metadata and return them
+		$image_data['width'] = $image_data['sizes']['huge']['width'];
+		$image_data['height'] = $image_data['sizes']['huge']['height'];
+		unset($image_data['sizes']['huge']);
+
+		// Check if other size-configurations link to the large-file
+		foreach($image_data['sizes'] as $size => $sizeData) {
+		  if ($sizeData['file'] === $large_image_filename)
+		      unset($image_data['sizes'][$size]);
+		}
+
+		return $image_data;
+	}
+	add_filter('wp_generate_attachment_metadata', 'replace_uploaded_image');
 
 
 // Register Menus
