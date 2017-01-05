fe396962121d4750cc45af1d08442629b21ea083
raidho-mx <miranda@raidho.mx>
2017-01-05 16:38:15 -0600

    cuando se filtra por tipo de categoría, se pierde el filtro, únicamente aparece el título de la categoría

--- inc/breadcrumbs.php
+++ inc/breadcrumbs.php
@@ -5,27 +5,47 @@
 		<li><a href="#">Inicio</a></li><?php
 
 	if(is_category()) { ?>
+
 		<li><a href="#" class="active"><?php single_cat_title(); ?></a></li><?php
+
+
+	} elseif(is_tax()) {
+		$obj = get_post_type_object( get_post_type() );
+		$catID = get_queried_object_id(); ?>
+
+		<li><a href="<?php get_post_type_archive_link($obj->name); ?>"><?php echo $obj->label; ?></a></li>
+		<li><a href="#" class="active"><?php echo get_cat_name($catID); ?></a></li><?php
+
+
 	} elseif(is_archive()) { ?>
+
 		<li><a href="#" class="active"><?php post_type_archive_title(); ?></a></li><?php
+
+
 	} else {
 		if(is_singular( array( 'proyectos', 'eventos' ))) {
 			$obj = get_post_type_object( get_post_type($post->ID) );
 			$pTypes = wp_get_post_terms(get_the_id(), $obj->taxonomies[0]); ?>
-			<li><a href="<?php echo home_url($obj->name); ?>"><?php echo $obj->label; ?></a></li><?php
+
+		<li><a href="<?php echo home_url($obj->name); ?>"><?php echo $obj->label; ?></a></li><?php
+
 			foreach( $pTypes as $pType ) { ?>
-			<li><a href="<?php echo get_term_link($pType->term_id) ?>" title="<?php echo $pType->name ?>"><?php echo $pType->name; ?></a></li><?php
+
+		<li><a href="<?php echo get_term_link($pType->term_id) ?>" title="<?php echo $pType->name ?>"><?php echo $pType->name; ?></a></li><?php
 			}
 
 
 		} elseif(is_single()) { ?>
-			<li><?php the_category(' '); ?></li><?php
+
+		<li><?php the_category(' '); ?></li><?php
 
 
 		} else {
 			echo 'no info';
 		} ?>
 		<li><a href="<?php the_permalink(); ?>" class="active"><?php the_title(); ?></a></li><?php
+
+
 	} ?>
 	</ul>
 </div>
