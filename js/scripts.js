a4c6e4f62736e10e5bc3d388dd4d2d73295dc318
raidho-mx <miranda@raidho.mx>
2017-01-09 20:10:32 -0600

    Up to date with Linda

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,11 @@
 			$(this).toggleClass('openStDropdown');
 		});
 
+// TRIGGER DROPDOWN FORM FIXED MENU
+		$('#fixMenu .burger').click(function(){
+			$('#fixMenu').toggleClass('show');
+		});
+
 
 // GALLERY SLIDER
 		$('.img-video-slider .slider').slick({
@@ -216,178 +221,55 @@
 			}
 		});
 
-		var controller = $.superscrollorama();
-		controller.pin($('#vectorStroke'), 4000, {
-			anim: (new TimelineLite())
-			.append(
-				TweenMax.fromTo($('#cir1'), 1,
-					{css:{'margin-top' : '-50px'}, immediateRender:true},
-					{css:{'margin-top' : '50px'}})
-			),
-			// .append(
-			// 	TweenMax.to($('#cir1'), 1,
-			// 	{css:{left: 200}})
-			// ),
-			onPin: function() {
-				$('#vectorStroke').addClass('show');
-				$(window).scroll(startCounter);
-				function startCounter() {
-					if ($(window).scrollTop() > 200) {
-						$(window).off("scroll", startCounter);
-						$('.count').each(function () {
-							var $this = $(this);
-							jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-								duration: 1000,
-								easing: 'swing',
-								step: function () {
-									$this.text(Math.ceil(this.Counter));
-								}
-							});
-						});
-					}
-				}
-			},
-			onUnpin: function() {
-				$('#vectorStroke').removeClass('show');
-			}
-		});
-		controller.pin($('#vectorPath'), 4000, {
-			// anim: (new TimelineLite())
-			// .append(
-			// 	TweenMax.to($('#cir2'), 1,
-			// 	{css:{'margin-right': '50px'}},
-			// 	{css:{'margin-right': '-120px'}})
-			// ),
-			onPin: function() {
-				$('#vectorPath').addClass('show');
+		//FADE MENU ON SCROLL
+		var vectorPath = $('#fixMenu').offset().top;
+		var map1 = $('#map1').offset().top - 300;
+		var map2 = $('#map2').offset().top - 300;
+		var blockIcons = $('#blockIcons').offset().top - 300;
+		var blockHab = $('#blockHab').offset().top + 300;
+		var blockInfo = $('#blockInfo').offset().top + 300;
+		var blockInfo2 = $('#blockInfo').offset().top + 1200;
+		$(window).scroll(function() {
+			var top = $(this).scrollTop();
+
+			if ( top > vectorPath ) {
+				$('#fixMenu').addClass('fixtop');
+			} if ( top < vectorPath ) {
+				$('#fixMenu').removeClass('fixtop');
+			} if ( top > map1 ) {
+				$('#map1').addClass('show');
+				$('#cir').addClass('show');
+			} if ( top < map1 ) {
+				$('#map1').removeClass('show');
+				$('#cir').removeClass('show');
+			} if ( top > map2 ) {
+				$('#map2').addClass('show');
 				$('#cir2').addClass('show');
-
-				var loader     = new TimelineMax({ repeat: 0, yoyo: true }),
-				svglength  = $('#vectorPath svg polygon').length,
-				bubbles    = [];
-
-				for(var i = 1, l = svglength; i <= l; i++) {
-					bubbles.push($('#vectorPath polygon:nth-of-type('+ i +'), #vectorPath path:nth-of-type('+ i +')'));
-				}
-
-				loader.staggerTo(bubbles, 0.1, {
-					css : {
-						fill: '#396FB6',
-						opacity: 1
-					}
-				}, 0.1);
-
-				$(window).scroll(startCounter);
-				function startCounter() {
-					if ($(window).scrollTop() > 200) {
-						$(window).off("scroll", startCounter);
-						$('.count').each(function () {
-							var $this = $(this);
-							jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-								duration: 1000,
-								easing: 'swing',
-								step: function () {
-									$this.text(Math.ceil(this.Counter));
-								}
-							});
-						});
-					}
-				}
-			},
-			onUnpin: function() {
-				$('#vectorPath').removeClass('show');
+			} if ( top < map2 ) {
+				$('#map2').removeClass('show');
 				$('#cir2').removeClass('show');
-
-				var loader     = new TimelineMax({ repeat: 1, yoyo: true }),
-				svglength  = $('#vectorPath svg polygon').length,
-				bubbles    = [];
-
-				for(var i = 1, l = svglength; i <= l; i++) {
-					bubbles.push($('#vectorPath polygon:nth-of-type('+ i +'), #vectorPath path:nth-of-type('+ i +')'));
-				}
-
-				loader.staggerTo(bubbles, 0, {
-					css : {
-						fill: '#396FB6',
-						opacity: 0
-					}
-				}, 0);
+			} if ( top > blockIcons ) {
+				$('#blockIcons .cir-img-x2').addClass('show');
+			} if ( top < blockIcons ) {
+				$('#blockIcons .cir-img-x2').removeClass('show');
+			} if ( top > blockHab ) {
+				$('#blockHab .cir-img-x2').addClass('show');
+			} if ( top < blockHab ) {
+				$('#blockHab .cir-img-x2').removeClass('show');
+			} if ( top > blockInfo) {
+				$('#blockInfo').addClass('show');
+			} if ( top < blockInfo) {
+				$('#blockInfo').removeClass('show');
+			} if ( top > blockInfo2) {
+				$('#blockInfo2').addClass('show');
+			} if ( top < blockInfo2) {
+				$('#blockInfo2').removeClass('show');
 			}
+			console.log(blockInfo);
+			console.log(blockIcons);
+			console.log(blockHab);
+			console.log(top);
 		});
-		controller.pin($('#vectorIcons'), 4000, {
-			onPin: function () {
-				// $('#vectorIcons ul > li').each(function(i) {
-				// 	$(this).fadeOut(0).delay(20*i).fadeIn(150);
-				// });
-				$('#cir3').addClass('show');
-
-				// ANIMATE LISTED ICONS
-				var v = $("#vectorIcons ul > li").css('visibility', 'hidden'), cur = 0;
-				for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
-				function fadeInNextLI() {
-					v.eq(cur++).css({'visibility':'visible'}).hide().fadeIn();
-					if(cur != v.length) setTimeout(fadeInNextLI, 3);
-				}
-				fadeInNextLI();
-
-				// ANIMATE COUNTER
-				$(window).scroll(startCounter);
-				function startCounter() {
-					if ($(window).scrollTop() > 200) {
-						$(window).off("scroll", startCounter);
-						$('.count').each(function () {
-							var $this = $(this);
-							jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-								duration: 1000,
-								easing: 'swing',
-								step: function () {
-									$this.text(Math.ceil(this.Counter));
-								}
-							});
-						});
-					}
-				}
-			},
-			onUnpin: function() {
-				$('#cir3').removeClass('show');
-			}
-		});
-		controller.pin($('#vectorHab'), 4000, {
-			onPin: function () {
-				$('#cir3').addClass('show');
-
-				// ANIMATE LISTED ICONS
-				var v = $("#vectorHab ul > li").css('visibility', 'hidden'), cur = 0;
-				for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
-				function fadeInNextLI() {
-					v.eq(cur++).css('visibility','visible').hide().fadeIn();
-					if(cur != v.length) setTimeout(fadeInNextLI, 3);
-				}
-				fadeInNextLI();
-
-				// ANIMATE COUNTER
-				$(window).scroll(startCounter);
-				function startCounter() {
-					if ($(window).scrollTop() > 200) {
-						$(window).off("scroll", startCounter);
-						$('.count').each(function () {
-							var $this = $(this);
-							jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-								duration: 1000,
-								easing: 'swing',
-								step: function () {
-									$this.text(Math.ceil(this.Counter));
-								}
-							});
-						});
-					}
-				}
-			},
-			onUnpin: function() {
-				$('#cir3').removeClass('show');
-			}
-		});
-
 
 
 		var $newdiv = $('<li><img src="img/icon-person.svg" alt=""></li>');
---
+++ page-acerca.php
@@ -0,0 +1,371 @@
+<?php
+
+	get_header();
+	get_template_part('inc/breadcrumbs'); ?>
+
+
+<section class="ptb100">
+	<div class="wrap">
+		<div class="two-col c-center t-center">
+			<h1><strong>DistritoTec</strong> es el lugar donde se viven las grandes ideas.</h1>
+		</div>
+	</div>
+</section>
+
+
+<section class="fixed-menu bg-skyblue" id="fixMenu">
+	<div class="burger">Menu</div>
+	<div class="st-menu">
+		<ul>
+			<li><a href="#blockInfo">DistritoTec</a></li>
+			<li><a href="#block2">¿Por Qué?</a></li>
+			<li><a href="#block3">Historia</a></li>
+			<li><a href="#block4">¿Qué buscamos? </a></li>
+			<li><a href="#block5">Proyectos</a></li>
+			<li><a href="#block6">Activaciones</a></li>
+		</ul>
+	</div>
+</section>
+
+<div class="hero-fixed" style="background-image: url('img/about1.jpg');"></div>
+
+<section class="ptb100">
+	<div class="wrap">
+		<div class="two-col c-center t-center">
+			<h1>Nos ubicamos al sur de la ciudad de Monterrey, en un polígono de <strong>452</strong> hectáreas.</h1>
+		</div>
+	</div>
+</section>
+
+<!-- AQUI IBA LO DEL MARKUP -->
+
+<!-- FULL VIDEO W/TITLING BLOCK -->
+<div class="pb100" id="block2">
+	<div class="t-center ptb100 bg-sand">
+		<div class="wrap">
+			<p><strong>¿Por Qué?</strong></p>
+			<h1>¿Cómo llegamos hasta aquí?</h1>
+		</div>
+	</div>
+	<div class="full-video-wrap">
+		<iframe width="100%" height="500" src="https://www.youtube.com/embed/wXr9jWolLRE" frameborder="0" allowfullscreen=""></iframe>
+	</div>
+	<div class="full-video-cap wrap mt20"><p>*Imagenes NASA Google Earth 1980 - 2016.</p></div>
+</div>
+
+<!-- FOURTHS ONLY TXT BLOCK -->
+<section class="pb100 t-center" id="block3">
+	<div class="med-wrap c-center pb100">
+		<h1>La población urbana ha crecido de manera acelerada en todo el mundo.</h1>
+	</div>
+	<div class="wrap">
+		<div class="one-fourth columns">
+			<h3>4,000 millones de personas viven en ciudades.</h3>
+		</div>
+		<div class="one-fourth columns">
+			<h3>Más de la mitad de la población mundial</h3>
+		</div>
+		<div class="one-fourth columns">
+			<h3>En 2050 podría crecer hasta un 66%</h3>
+		</div>
+	</div>
+</section>
+
+<!-- THIRDS W/THUMBNAILS BLOCK -->
+<section class="pb100 t-center">
+	<div class="med-wrap c-center pb100">
+		<h1>Esto representa una serie de retos ambientales, sociales, políticos, económicos y financieros. </h1>
+	</div>
+	<div class="wrap square-thumbnails">
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>México es uno de los países más urbanizados de América Latina.</h3>
+				</div>
+			</a>
+		</div>
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>78% de la población vive en ciudades.</h3>
+				</div>
+			</a>
+		</div>
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>50% se concentra en 57 zonas metropolitanas.</h3>
+				</div>
+			</a>
+		</div>
+	</div>
+</section>
+
+<!-- FULL VIDEO BLOCK -->
+<section class="pb100">
+	<div class="med-wrap c-center pb100 t-center">
+		<h1>El crecimiento urbano exponencial = una expansión desordenada de las zonas metropolitanas.</h1>
+	</div>
+	<div class="full-video-wrap">
+		<iframe width="100%" height="500" src="https://www.youtube.com/embed/wXr9jWolLRE" frameborder="0" allowfullscreen=""></iframe>
+	</div>
+	<div class="full-video-cap wrap mt20"><p>*Imagenes NASA Google Earth 1980 - 2016.</p></div>
+</section>
+
+<!-- FOURTHS ONLY CENTERED TXT BLOCK -->
+<section class="pb100 t-center">
+	<div class="med-wrap c-center pb100">
+		<h1>Monterrey no fue la excepción.</h1>
+		<h3 class="mt20">De 1980 a 2016:</h3>
+	</div>
+	<div class="wrap">
+		<div class="one-fourth columns">
+			<h3>La población se duplicó (2X) </h3>
+		</div>
+		<div class="one-fourth columns">
+			<h3>La mancha urbana se quintuplicó (5X)</h3>
+		</div>
+	</div>
+</section>
+
+<!-- THIRDS W/THUMBNAILS BLOCK -->
+<section class="pb100 t-center">
+	<div class="med-wrap c-center pb100">
+		<h1>DistritoTec, así como otras zonas en la ciudad, enfrenta una serie de retos.</h1>
+		<h3 class="mt20">De 2000 a 2010:</h3>
+	</div>
+	<div class="wrap square-thumbnails">
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>22% de disminución de población</h3>
+				</div>
+			</a>
+		</div>
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>36% de viviendas deshabitadas</h3>
+				</div>
+			</a>
+		</div>
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>Baja densidad (38 habitantes por hectárea)</h3>
+				</div>
+			</a>
+		</div>
+	</div>
+</section>
+
+<!-- HALFS W/THUMBNAILS BLOCK -->
+<section class="pb100">
+	<div class="med-wrap c-center t-center pb100">
+		<h1>Es momento de repensar el rol que tienen las ciudades.</h1>
+	</div>
+	<div class="wrap odds">
+		<div class="mb40">
+			<div class="two-col columns">
+				<div class="video-wrap">
+					<iframe width="100%" height="500" src="https://www.youtube.com/embed/wXr9jWolLRE" frameborder="0" allowfullscreen=""></iframe>
+				</div>
+			</div>
+			<div class="two-col columns">
+				<div class="table">
+					<div class="table-cell"><h2 class="c-blue"><strong>Salvador Alva</strong></h2><p>Presidente del Tecnológico de Monterrey</p></div>
+				</div>
+			</div>
+		</div>
+		<div class="mb40">
+			<div class="two-col columns">
+				<div class="video-wrap">
+					<iframe width="100%" height="500" src="https://www.youtube.com/embed/wXr9jWolLRE" frameborder="0" allowfullscreen=""></iframe>
+				</div>
+			</div>
+			<div class="two-col columns">
+				<div class="table">
+					<div class="table-cell"><h2 class="c-blue"><strong>David Noel Ramírez</strong></h2><p>Rector del Tecnológico de Monterrey</p></div>
+				</div>
+			</div>
+		</div>
+		<div class="mb40">
+			<div class="two-col columns">
+				<div class="video-wrap">
+					<iframe width="100%" height="500" src="https://www.youtube.com/embed/wXr9jWolLRE" frameborder="0" allowfullscreen=""></iframe>
+				</div>
+			</div>
+			<div class="two-col columns">
+				<div class="table">
+					<div class="table-cell"><h2 class="c-blue"><strong>José Antonio Torre</strong></h2><p>Director de Urbanismo Ciudadano Tecnológico de Monterrey</p></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- TIMELINE BLOCK -->
+<section>
+	<div class="wrap timeline-block">
+		<div class="three-col">
+			<div class="two-col mb40">
+				<h2 class="c-blue"><strong>Historia</strong></h2></br>
+				<p>El 13 de septiembre inició la operación del Aula de la Obra Rotonda Garza Sada, un proyecto educativo operado en el DistritoTec por la fundación Construyendo a México Crecemos. Este proyecto tiene como propósito apoyar la educación de los trabajadores de la construcción que laboran en la rehabilitación de la Rotonda Garza Sada. </p>
+			</div>
+		</div>
+		<ul>
+			<li>
+				<h2>2017</h2>
+				<figure>
+					<img src="http://placehold.it/950x550" alt="Render: Rotonda 1">
+					<figcaption>
+						<p class="m20 c-blue"><b>Render:</b>Rotonda 1</p>
+						<p>El 13 de septiembre inició la operación del Aula de la Obra Rotonda Garza Sada,
+							un proyecto educativo operado en el DistritoTec por la fundación Construyendo a México Crecemos.
+							Este proyecto tiene como propósito apoyar la educación de los trabajadores de la
+							construcción que laboran en la rehabilitación de la Rotonda Garza Sada.</p>
+					</figcaption>
+				</figure>
+			</li>
+			<li>
+				<h2>2017</h2>
+				<figure>
+					<img src="http://placehold.it/950x550" alt="Render: Rotonda 1">
+					<figcaption>
+						<p class="m20 c-blue"><b>Render:</b>Rotonda 2</p>
+						<p>El 13 de septiembre inició la operación del Aula de la Obra Rotonda Garza Sada,
+							un proyecto educativo operado en el DistritoTec por la fundación Construyendo a México Crecemos.
+							Este proyecto tiene como propósito apoyar la educación de los trabajadores de la construcción que
+							laboran en la rehabilitación de la Rotonda Garza Sada.</p>
+					</figcaption>
+				</figure>
+			</li>
+			<li>
+				<h2>2017</h2>
+				<figure>
+					<img src="http://placehold.it/950x550" alt="Render: Rotonda 1">
+					<figcaption>
+						<p class="m20 c-blue"><b>Render:</b>Rotonda 3</p>
+						<p>El 13 de se</p>
+					</figcaption>
+				</figure>
+			</li>
+			<li>
+				<h2>2017</h2>
+				<figure>
+					<img src="http://placehold.it/950x550" alt="Render: Rotonda 1">
+					<figcaption>
+						<p class="m20 c-blue"><b>Render:</b>Rotonda 4</p>
+						<p>El 13 de septiembre inició la operación del Aula de la Obra Rotonda Garza Sada,
+							un proyecto educativo operado en el DistritoTec por la fundación Construyendo a
+							México Crecemos. Este proyecto tiene como propósito apoyar la educación de los
+							trabajadores de la construcción que laboran en la rehabilitación de la Rotonda Garza Sada.</p>
+					</figcaption>
+				</figure>
+			</li>
+		</ul>
+	</div>
+</section>
+
+<!-- PROJECT TYPES BLOCK -->
+<section class="ft-project">
+	<div class="wrap info">
+		<div class="two-col">
+			<p class="mb20"><strong>Proyectos</strong></p>
+			<h1>Los tres grandes ejes de DistritoTec:</h1>
+			<p class="small-txt mb40"><strong>Estos son  ejemplos de los</br> 3 tipos de proyectos:</strong></p>
+		</div>
+		<div class="two-col mt40">
+			<ul class="project-type">
+				<li>Evolución del Campus</li>
+				<li>Mejora del Entorno</li>
+				<li>Clúster Tecnológico</li>
+			</ul>
+		</div>
+	</div>
+	<div class="wrap thumbnail-fourths">
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('http://placehold.it/750x750');"></div>
+				<div class="txt">
+					<h3>Teatro de la Ciudad</h3>
+					<p class="small-txt c-red mb20 type"><span class="bg-red">1</span><b>Mejora del Entorno</b></p>
+					<div class="marquee"><p class="small-txt"><b>Todas las Colonias#1, Todas las Colonias#2, Todas las Colonias#3, Todas las Colonias#4, Todas las Colonias#5, Todas las Colonias#6</b></p></div>
+				</div>
+				<div class="thumb-progress-bar red">
+					<p><b>80%</b> Completado</p>
+					<div style="width:80%;"></div>
+				</div>
+			</a>
+		</div>
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('http://placehold.it/750x750');"></div>
+				<div class="txt">
+					<h3>Teatro de la Ciudad</h3>
+					<p class="small-txt c-yellow mb20 type"><span class="bg-yellow">2</span><b>Mejora del Entorno</b></p>
+					<div class="marquee"><p class="small-txt"><b>Todas las Colonias#1, Todas las Colonias#2, Todas las Colonias#3, Todas las Colonias#4, Todas las Colonias#5, Todas las Colonias#6</b></p></div>
+				</div>
+				<div class="thumb-progress-bar yellow">
+					<p><b>80%</b> Completado</p>
+					<div style="width:80%;"></div>
+				</div>
+			</a>
+		</div>
+		<div class="one-third columns">
+			<a href="#">
+				<div class="img" style="background-image: url('http://placehold.it/750x750');"></div>
+				<div class="txt">
+					<h3>Teatro de la Ciudad</h3>
+					<p class="small-txt c-blue mb20 type"><span class="bg-blue">3</span><b>Mejora del Entorno</b></p>
+					<div class="marquee"><p class="small-txt"><b>Todas las Colonias#1, Todas las Colonias#2, Todas las Colonias#3, Todas las Colonias#4, Todas las Colonias#5, Todas las Colonias#6</b></p></div>
+				</div>
+				<div class="thumb-progress-bar blue">
+					<p><b>80%</b> Completado</p>
+					<div style="width:80%;"></div>
+				</div>
+			</a>
+		</div>
+	</div>
+</section>
+
+<section class="bg-sand ptb100" id="block4">
+	<div class="med-wrap c-center t-center">
+		<p class="c-skyblue"><strong>¿Qué buscamos?</strong></p>
+		<h1 class="mb20">Con una visión transformadora, lo que buscamos es generar:</h1>
+	</div>
+	<div class="wrap square-thumbnails tornado">
+		<div>
+			<div class="one-third">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>México es uno de los países más urbanizados de América Latina.</h3>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="one-third">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>México es uno de los países más urbanizados de América Latina.</h3>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="one-third">
+				<div class="img" style="background-image: url('img/map-bg.png');"></div>
+				<div class="txt">
+					<h3>México es uno de los países más urbanizados de América Latina.</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
