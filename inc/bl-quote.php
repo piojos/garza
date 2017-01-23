882ebd0339b7165b797fbc8bf088e7f93a5d8ad2
raidho-mx <miranda@raidho.mx>
2017-01-23 17:15:33 -0600

    Updated markup

--- inc/bl-quote.php
+++ inc/bl-quote.php
@@ -1,7 +1,7 @@
 <!-- HIGHLIGHTED TEXT BLOCK-->
 <section class="bg-sand">
 	<div class="wrap">
-		<div class="three-col">
+		<div class="big-wrap">
 			<h1 class="huge-txt"><?php the_sub_field('text'); ?></h1>
 		</div>
 	</div>
